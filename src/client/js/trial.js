a9341223c8f5f5b7e4368a06f2ea8048a192da70
Michele Zanella <zanella.michele@gmail.com>
2015-05-31 15:50:41 +0200

    [js] Fixed loading of single instructor page
    deleted- click event in all instructors page;
    added function to parse URL and obtain instructor's id
    
    Signed-off-by: Michele Zanella <zanella.michele@gmail.com>

--- src/client/js/trial.js
+++ src/client/js/trial.js
@@ -3,39 +3,6 @@ $(document).ready(documentReady);
 function documentReady(){
     console.log("I'm ready");
     getAllInstructors();
-    $("body").on("click",".loadmore",function(){
-        var id=$(this).attr("id"); //Saving the id of the link clicked
-        loadMoreClicked(id);
-    });
-
-}
-
-
-function loadMoreClicked(id){
-
-    console.log("You clicked instructor "+id.toString());
-
-    document.title="Instructor "+id.toString();
-    window.location.replace("instructor.html?instr="+id); //Redirect to id-th instructor
-    /*$.ajax({
-        method: "POST",
-        //dataType: "json", //type of data
-        crossDomain: true, //localhost purposes
-        url: "../../server/server.php", //Relative or absolute path to file.php file
-        data: {id:id},
-        success: function(response) {
-            console.log(JSON.parse(response));
-            var instructors=JSON.parse(response);
-            for(var i=0;i<instructors.length;i++){
-                console.log(instructors[i].Name);
-                $(".contents").html(" "+instructors[i].Name);
-            }
-        1},
-        error: function(request,error) 
-        {
-            console.log("Error");
-        }
-    });*/
 }
 
 function getAllInstructors(){
@@ -54,7 +21,7 @@ function getAllInstructors(){
             for(var i=0;i<instructors.length;i++){
                 console.log(instructors[i].Name);
                 //$(".contents").html(" "+instructors[i].Name);
-                el+="<div class='instructor' id='"+instructors[i].Id+"'><h2>"+instructors[i].Name+"</h2>  <span>"+instructors[i].Surname+"</span><a href='#' class='loadmore' id='"+instructors[i].Id+"'>Load more</a></div>";
+                el+="<div class='instructor' id='"+instructors[i].Id+"'><h2>"+instructors[i].Name+"</h2>  <span>"+instructors[i].Surname+"</span><a href='instructor.html?instr="+instructors[i].Id+"' class='loadmore' id='"+instructors[i].Id+"'>Load more</a></div>";
             }
             $("body").html(el);
         1},
--- src/client/js/trial2.js
+++ src/client/js/trial2.js
@@ -2,16 +2,14 @@ $(document).ready(documentReady);
 
 function documentReady(){
     console.log("I'm ready");
-    //$(".loadmore").on("click",loadMoreClicked());
-    var qs = new Querystring();
-    var id = qs.get("Instr");
+    var id=getUrlVars()["instr"];
     loadInstructor(id);
 }
 
 
 function loadInstructor(id){
 
-    console.log("Loading all instructotrs...");
+    console.log("Loading instructotrs"+id.toString());
     
     document.title="Instructor "+id.toString();
     
@@ -37,4 +35,18 @@ function loadInstructor(id){
             console.log("Error");
         }
     });
+}
+
+// Read a page's GET URL variables and return them as an associative array.
+function getUrlVars()
+{
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for(var i = 0; i < hashes.length; i++)
+    {
+        hash = hashes[i].split('=');
+        vars.push(hash[0]);
+        vars[hash[0]] = hash[1];
+    }
+    return vars;
 }
