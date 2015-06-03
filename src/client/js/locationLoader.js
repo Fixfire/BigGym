e0c93740589640c0efc2d774d8feab9ef24394f9
Michele Zanella <zanella.michele@gmail.com>
2015-06-03 17:05:51 +0200

    [js] Dynamic location data retrieving
    Signed-off-by: Michele Zanella <zanella.michele@gmail.com>

--- src/client/js/locationLoader.js
+++ src/client/js/locationLoader.js
@@ -1,18 +1,43 @@
 function initialize() {
+    var lat, lng; //Coordinates
+    var url; //Map url
+    
+    $.ajax({
+        method: "POST",
+        crossDomain: true, //localhost purposes
+        url: "../../server/getLocation.php", //Relative or absolute path to file.php file
+        success: function(response) {
+            console.log(JSON.parse(response));
+            var location=JSON.parse(response);
+ 
+            for(var i=0;i<location.length;i++){
+                lat=location[i].Latitude;
+                lng=location[i].Longitude;
+                url=location[i].Url;
+                console.log(lat);
+                console.log(lng);
+            }
+        1},
+        error: function(request,error) 
+        {
+            console.log("Error");
+        }
+    });
+    
     var mapOptions = {
-        center: { lat: 37.091, lng: -76.478},
+        center: new google.maps.LatLng(lat, lng), //37.091 -76.478
         zoom: 16
     };
     var map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
-
+    
     // Creating a marker and positioning it on the map  
     var marker = new google.maps.Marker({  
-        position: new google.maps.LatLng(37.091, -76.478),  
+        position: new google.maps.LatLng(lat, lng),  
         title: 'Big Gym',
-        url: "https://www.google.it/maps/place/37%C2%B005'27.6%22N+76%C2%B028'40.8%22W/@37.091,-76.478,17z/data=!3m1!4b1!4m2!3m1!1s0x0:0x0?hl=en",
+        url: url,
         map: map  
     });
     google.maps.event.addListener(marker, 'click', function() {
-        window.location.href = "https://www.google.it/maps/place/37%C2%B005'27.6%22N+76%C2%B028'40.8%22W/@37.091,-76.478,17z/data=!3m1!4b1!4m2!3m1!1s0x0:0x0?hl=en";
+        window.location.href = url;
     });
 }
