fcfdf754af0548d8ca0a3e350dd903fabdd3490f
ubugu <claudio.ubugu@gmail.com>
2015-06-06 18:01:58 +0200

    [php] Added get course from name.

---
+++ src/server/getCourses_name.php
@@ -0,0 +1,25 @@
+<?php
+    include 'db_connector.php';
+
+    $coursey=$_POST["course"];
+   
+    # extract results mysqli_result::fetch_array
+    $query = "SELECT * FROM course WHERE course.Name = '".$course."'";
+    //query execution
+    $result = $mysqli->query($query);
+    //if there are data available
+    if($result->num_rows >0)
+    {
+        $myArray = array();//create an array
+        while($row = $result->fetch_array(MYSQL_ASSOC)) {
+            $myArray[] = array_map('utf8_encode', $row);	
+        }
+        echo json_encode($myArray);
+    }
+
+    //free result
+    $result->close();
+
+    //close connection
+    $mysqli->close();
+?>
