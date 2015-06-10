38478798aa3b8659df9f7af37c4d00124e758b16
Michele Zanella <zanella.michele@gmail.com>
2015-06-10 14:24:51 +0200

    [js] Fixed server call issue
    Signed-off-by: Michele Zanella <zanella.michele@gmail.com>

--- src/client/js/joinus.js
+++ src/client/js/joinus.js
@@ -9,25 +9,34 @@ function documentReady(){
         /* get some values from elements on the page: */
         var $form = $( this ),
         url = $form.attr( 'action' );
-        
         /* Send the data using post */
-        var posting = $.post( url, { appellative: $('#appellative').val(), 
-                                    surname: $('#surname').val(),
-                                    name: $('#name').val(),
-                                    birthdate: $('#birthdate').val(),
-                                    address: $('#address').val(),
-                                    city: $('#city').val(),
-                                    zip: $('#zip').val(),
-                                    country: $('#country').val(),
-                                    state: $('#state').val(),
-                                    phone: $('#phone').val(),
-                                    email: $('#email').val(),
-                                    weight: $('#weight').val(),
-                                    height: $('#height').val(),
-                                    paymentType: $('#paymentType').val(),
-                                    acceptance: $('#acceptance').val()
-                                   });
-        
-    
+        $.ajax( {
+            method: "POST",
+            crossDomain: true, //localhost purposes
+            url: url, 
+            data: { appellative: $('#appellative').val(), 
+                        surname: $('#surname').val(),
+                        name: $('#name').val(),
+                        birthdate: $('#birthdate').val(),
+                        address: $('#address').val(),
+                        city: $('#city').val(),
+                        zip: $('#zip').val(),
+                        country: $('#country').val(),
+                        state: $('#state').val(),
+                        phone: $('#phone').val(),
+                        email: $('#email').val(),
+                        weight: $('#weight').val(),
+                        height: $('#height').val(),
+                        paymentType: $('#paymentType').val(),
+                        acceptance: $('#acceptance').val()
+                    },
+            success: function(response) {
+                alert( "Registration succeded! An email will be send when you can come to our desk office and keep your membership card" );
+            },
+            error: function(request,error) 
+        {
+            console.log("Error");
+        }
+       });
     });
 }
